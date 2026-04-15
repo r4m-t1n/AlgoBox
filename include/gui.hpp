0cd92e4a46dac6968d13be77b41fab0a2d382fe2
r4m-t1n <im.r4mt1n@gmail.com>
2026-04-15 22:52:47 +0200

    feat: add screen for search

---
+++ include/gui.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <thread>
+#include <chrono>
+#include "raylib.h"
+#include "rlgl.h"
+#include "algobox_vector.hpp"
+
+template <typename T>
+bool draw_rectangles(const algobox::vector<T>& v,
+    const float& screen_height, const float& rectangle_width,
+    const T& target, const size_t& current_idx, const T& maximum){
+    bool target_reached = false;
+    for (size_t i=0; i<v.size(); i++){
+        float rectangle_height = ((float)v.at(i)/maximum) * screen_height;
+
+        float posX = (float)i * rectangle_width;
+        float posY = screen_height - rectangle_height;
+
+        Rectangle rec = {posX, posY, rectangle_width - 1.0f, rectangle_height};
+
+        Color rectangle_color;
+
+        if (current_idx == i){
+            if (v.at(current_idx) == target){
+                rectangle_color = GREEN;
+                DrawRectangleRec(rec, rectangle_color);
+                target_reached = true;
+            }
+            else{
+                rectangle_color = RED;
+            }
+        }
+        else{
+            rectangle_color = BLUE;
+        }
+
+        DrawRectangleRec(rec, rectangle_color);
+    }
+    return target_reached;
+}
+
+template <typename T>
+void screen_search(algobox::vector<T>& v, T target){
+    const float screen_height = 720.0f;
+    const float screen_width = 720.0f;
+
+    const float rectangle_width = screen_width / v.size();
+
+    InitWindow(screen_width, screen_height, "Search Algorithm");
+    SetTargetFPS(60);
+
+    T maximum = v.get_max();
+    size_t current_idx = 0;
+
+    bool target_reached = false;
+    while (!WindowShouldClose() && !target_reached){
+        
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        target_reached = draw_rectangles(
+            v, screen_height, rectangle_width, target, current_idx, maximum);
+            
+        EndDrawing();
+        v.nodes.pop();
+        
+        current_idx++;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    CloseWindow();
+
+}
