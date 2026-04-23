4ac75fe7d305e4de0cd47a88ae7ea15d2067dc15
r4m-t1n <im.r4mt1n@gmail.com>
2026-04-23 19:27:20 +0200

    refactor: use loop tracker

--- examples/linear_search.cpp
+++ examples/linear_search.cpp
@@ -3,16 +3,18 @@
 #include "gui.hpp"
 
 
-int linear_search(algobox::vector<int> v, int target){
+int linear_search(algobox::vector<int>& v, int target){
+    algobox::tracker loop(v);
     for (int i=0; i<v.size(); i++){
         if (v[i] == target) return i;
+        loop++;
     }
     return -1;
 }
 
 int main(){
-    algobox::vector<int> v = {1, 5, 7, 10, 12, 15, 17}; // it should be sorted first
-    int target = 10;
+    algobox::vector<int> v = {1, 5, 7, 10, 12, 15, 17, 20, 23, 26, 30, 40, 43}; // it should be sorted first
+    int target = 50;
     algobox::algo_search(linear_search, v, target);
     std::cin.get();
 }
