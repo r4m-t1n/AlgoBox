30601402a1f18f687a62118226d0361d257b67cf
r4m-t1n <im.r4mt1n@gmail.com>
2026-04-23 19:25:59 +0200

    feat: add loop_tracker, dict for indexes

--- include/algobox_vector.hpp
+++ include/algobox_vector.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 #include <queue>
 #include <thread>
 
 namespace algobox{
     template <typename T>
+    struct tracker;
+    template <typename T>
     class vector;
+    class dict;
     
     template <typename Func, typename... Args>
     auto algo_search(Func algorithm, Args&&... args);
 };
-
 
 template <typename T>
 void screen_search(algobox::vector<T>& v, T target);
@@ -36,6 +39,28 @@ auto algobox::algo_search(Func algorithm, Args&&... args){
     return result;
 }
 
+class algobox::dict {
+    public:
+        mutable std::unordered_map<std::string, std::queue<size_t>> data;
+
+        dict() : data() {};
+
+        size_t& operator[](const std::string& key){
+            std::queue<size_t>& val = data[key];
+            if (val.empty()){
+                val.push(0);
+            }
+            return val.back();
+        }
+
+        const size_t operator[](const std::string& key) const{
+            if (data.find(key) == data.end() || data[key].empty())
+                return 0;
+            return data[key].back();
+        }
+
+};
+
 template <typename T>
 class algobox::vector: public std::vector<T>{
     public:
@@ -59,13 +84,54 @@ class algobox::vector: public std::vector<T>{
         T& operator[](size_t index){
             T val = this->at(index);
 
-            nodes.push(index); // for now, to be implemented
+            nodes.back() = index; // for now, to be implemented
             return std::vector<T>::operator[](index);
         }
 
         const T& operator[](size_t index) const{
-            nodes.push(index);
+            nodes.back() = index;
 
             return std::vector<T>::operator[](index);
+        }
+};
+
+template <typename T>
+class algobox::tracker{
+    public:
+        size_t loop_index = 0;
+
+        tracker(algobox::vector<T>& v, algobox::dict& kv):
+            vector(v), vars(kv) {
+
+            add_empty_element();
+
+        }
+        tracker(algobox::vector<T>& v):
+            vector(v), vars(dummy_dict) {
+
+            add_empty_element_v();
+        }
+
+        void operator++(int){
+            loop_index++;            
+            add_empty_element();
+        }
+    private:
+        algobox::vector<T>& vector;
+        algobox::dict& vars;
+        static inline algobox::dict dummy_dict;
+
+        void add_empty_element(){
+            const size_t empty_element = static_cast<size_t>(-1);
+            for (auto& [key, val]: vars.data){
+                val.push(empty_element);
+            }
+            vector.nodes.push(empty_element);
+        }
+
+        void add_empty_element_v(){
+            const size_t empty_element = static_cast<size_t>(-1);
+
+            vector.nodes.push(empty_element);
         }
 };
