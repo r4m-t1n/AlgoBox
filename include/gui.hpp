8e3c16666403a9c97411709b207d458da0dcf7aa
r4m-t1n <im.r4mt1n@gmail.com>
2026-04-30 17:27:08 +0200

    feat: add draw_arrows to indicate highlighted rectangle

--- include/gui.hpp
+++ include/gui.hpp
@@ -6,9 +6,26 @@
 #include "rlgl.h"
 #include "algobox_vector.hpp"
 
-template <typename T>
-void draw_arrows(const algobox::vector<T>& v, const size_t& current_idx){
-
+void draw_arrows(const float& posX, const float& posY,
+    const float& rectangle_width, Color rectangle_color){
+    float base_y = posY - 45.0f;
+    float bisector_x = posX + ((rectangle_width - 1.0f)/2);
+    float bisector_y = posY - 20.0f;
+    Vector2 left_top = {posX, base_y};
+    Vector2 middle_bot = {bisector_x, bisector_y};
+    Vector2 right_top = {posX + rectangle_width - 1.0f, base_y};
+    DrawTriangle(
+        left_top,
+        middle_bot,
+        right_top,
+        rectangle_color
+    );
+    DrawLineEx(
+        (Vector2) {bisector_x, base_y},
+        (Vector2){bisector_x, base_y-20.0f},
+        (rectangle_width - 1.0f)/4,
+        BLACK
+    );
 }
 
 template <typename T>
@@ -20,7 +37,7 @@ bool draw_rectangles(const algobox::vector<T>& v,
         float rectangle_height = ((float)v.at(i)/maximum) * screen_height;
 
         float posX = (float)i * rectangle_width;
-        float posY = screen_height - rectangle_height;
+        float posY = screen_height - rectangle_height + (rectangle_height / 5);
 
         Rectangle rec = {posX, posY, rectangle_width - 1.0f, rectangle_height};
 
@@ -35,6 +52,7 @@ bool draw_rectangles(const algobox::vector<T>& v,
             else{
                 rectangle_color = RED;
             }
+            draw_arrows(posX, posY, rectangle_width, rectangle_color);
         } else{
             rectangle_color = BLUE;
         }
