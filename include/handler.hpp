a1c9b1c47a92029a7cdff1e889442aab92ff6b70
r4m-t1n <im.r4mt1n@gmail.com>
2026-05-02 21:43:05 +0200

    feat: add algo_sort to handle sorting funcs

--- include/handler.hpp
+++ include/handler.hpp
@@ -5,18 +5,43 @@
 
 template <typename Func, typename... Args>
 auto algobox::algo_search(Func algorithm, Args&&... args){
-    auto result = algorithm(std::forward<Args>(args)...);
-
+    
     auto params = std::forward_as_tuple(std::forward<Args>(args)...);
-
+    
     using first_arg = std::decay_t<std::tuple_element_t<0, std::tuple<Args...>>>;
     using T = typename first_arg::value_type;
-
+    
     algobox::core<T>& c = std::get<0>(params);
     auto target = std::get<1>(params);
 
+    c.v.mode = SEARCH;
+    
+    auto result = algorithm(std::forward<Args>(args)...);
+
     std::thread gui_thread([&c, target](){
         screen_search<T>(c, target);
+    });
+    gui_thread.detach();
+
+    return result;
+}
+
+template <typename Func, typename... Args>
+auto algobox::algo_sort(Func algorithm, Args&&... args){
+    
+    auto params = std::forward_as_tuple(std::forward<Args>(args)...);
+    
+    using first_arg = std::decay_t<std::tuple_element_t<0, std::tuple<Args...>>>;
+    using T = typename first_arg::value_type;
+    
+    algobox::core<T>& c = std::get<0>(params);
+
+    c.v.mode = SORT;
+
+    auto result = algorithm(std::forward<Args>(args)...);
+
+    std::thread gui_thread([&c](){
+        screen_sort<T>(c);
     });
     gui_thread.detach();
 
