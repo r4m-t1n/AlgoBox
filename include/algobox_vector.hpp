101bc91f49f55f0a4d03949ea61460f2ea9ec41d
r4m-t1n <im.r4mt1n@gmail.com>
2026-05-02 21:44:14 +0200

    feat: add a new queue to save the states of each loop, add swap

--- include/algobox_vector.hpp
+++ include/algobox_vector.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <queue>
+#include <unordered_map>
 #include "algobox.hpp"
 
 
@@ -10,9 +11,12 @@ class algobox::vector: public std::vector<T>{
     public:
         static_assert(std::is_arithmetic_v<T>, "You should use number data types.");
 
+        AlgoMode mode;
+
         using std::vector<T>::vector;
 
         mutable std::queue<size_t> nodes;
+        mutable std::queue<algobox::State<T>> copies;
 
         T get_max(){
             if (this->empty())
@@ -25,17 +29,43 @@ class algobox::vector: public std::vector<T>{
             return m;
         }
 
+        void swap(size_t index_1, size_t index_2){
+            if (index_1 >= this->size() || index_2 >= this->size())
+                return;
+
+            size_t tmp = this->at(index_1);
+            size_t tmp_2 = this->at(index_2);
+            this->at(index_1) = this->at(index_2);
+            this->at(index_2) = tmp;
+            copies.emplace(std::vector<T>(*this), index_2, index_2, index_1); // green, red
+        }
+
         T& operator[](size_t index){
-            if (nodes.empty()){
-                nodes.push(index);
+            switch (mode) {
+                case SEARCH:
+                    if (nodes.empty()){
+                        nodes.push(index);
+                    }
+                    nodes.back() = index;
+                    break;
+                
+                case SORT:
+                    size_t empty_element = static_cast<size_t>(-1);
+                    copies.emplace(std::vector<T>(*this), index, empty_element, empty_element);
+                    break;
             }
-            nodes.back() = index; // for now, to be implemented
             return std::vector<T>::operator[](index);
         }
 
         const T& operator[](size_t index) const{
-            nodes.back() = index;
-
+            switch (mode) {
+                case SEARCH:
+                    if (nodes.empty()){
+                        nodes.push(index);
+                    }
+                    nodes.back() = index;
+                    break;
+            }
             return std::vector<T>::operator[](index);
         }
 };
