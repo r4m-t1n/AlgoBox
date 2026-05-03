c6e48a6b42d83a43a769e509bba9c54ff20e2ade
r4m-t1n <im.r4mt1n@gmail.com>
2026-05-03 11:57:05 +0200

    refactor: move definitions outside of class

--- include/algobox_vector.hpp
+++ include/algobox_vector.hpp
@@ -18,56 +18,69 @@ class algobox::vector: public std::vector<T>{
         mutable std::queue<size_t> nodes;
         mutable std::queue<algobox::State<T>> copies;
 
-        T get_max(){
-            if (this->empty())
-                return 0;
+        T get_max();
 
-            T m = this->at(0);
-            for (int i=0; i<this->size(); i++){
-                if (this->at(i) > m) m = this->at(i);
-            }
-            return m;
-        }
+        void swap(size_t index_1, size_t index_2);
 
-        void swap(size_t index_1, size_t index_2){
-            if (index_1 >= this->size() || index_2 >= this->size())
-                return;
+        T& operator[](size_t index);
 
-            T tmp = this->at(index_1);
-            T tmp_2 = this->at(index_2);
-            this->at(index_1) = this->at(index_2);
-            this->at(index_2) = tmp;
-            copies.emplace(std::vector<T>(*this), index_2, index_1, index_2); // green, red
-        }
-
-        T& operator[](size_t index){
-            switch (mode) {
-                case SEARCH:
-                    if (nodes.empty()){
-                        nodes.push(index);
-                    }
-                    nodes.back() = index;
-                    break;
-
-                case SORT:
-                    if (!copies.empty() && copies.back().index == index)
-                        break;
-                    copies.emplace(std::vector<T>(*this), index,
-                    algobox::core<T>::empty_element, algobox::core<T>::empty_element);
-                    break;
-            }
-            return std::vector<T>::operator[](index);
-        }
-
-        const T& operator[](size_t index) const{
-            switch (mode) {
-                case SEARCH:
-                    if (nodes.empty()){
-                        nodes.push(index);
-                    }
-                    nodes.back() = index;
-                    break;
-            }
-            return std::vector<T>::operator[](index);
-        }
+        const T& operator[](size_t index) const;
 };
+
+
+template <typename T>
+T algobox::vector<T>::get_max(){
+    if (this->empty())
+        return 0;
+
+    T m = this->at(0);
+    for (int i=0; i<this->size(); i++){
+        if (this->at(i) > m) m = this->at(i);
+    }
+    return m;
+}
+
+template <typename T>
+void algobox::vector<T>::swap(size_t index_1, size_t index_2){
+    if (index_1 >= this->size() || index_2 >= this->size())
+        return;
+
+    T tmp = this->at(index_1);
+    T tmp_2 = this->at(index_2);
+    this->at(index_1) = this->at(index_2);
+    this->at(index_2) = tmp;
+    copies.emplace(std::vector<T>(*this), index_2, index_1, index_2); // green, red
+}
+
+template <typename T>
+T& algobox::vector<T>::operator[](size_t index){
+    switch (mode) {
+        case SEARCH:
+            if (nodes.empty()){
+                nodes.push(index);
+            }
+            nodes.back() = index;
+            break;
+
+        case SORT:
+            if (!copies.empty() && copies.back().index == index)
+                break;
+            copies.emplace(std::vector<T>(*this), index,
+            algobox::core<T>::empty_element, algobox::core<T>::empty_element);
+            break;
+    }
+    return std::vector<T>::operator[](index);
+}
+
+template <typename T>
+const T& algobox::vector<T>::operator[](size_t index) const{
+    switch (mode) {
+        case SEARCH:
+            if (nodes.empty()){
+                nodes.push(index);
+            }
+            nodes.back() = index;
+            break;
+    }
+    return std::vector<T>::operator[](index);
+}
